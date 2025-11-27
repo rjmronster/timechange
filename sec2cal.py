6020b4140cac9d87ab94346fedac5ecdd9b77197
Ronald Muellerschoen <muellerschoen@sbcglobal.net>
2025-11-26 16:00:57 -0800

    Update sec2cal.py
    change output description

--- sec2cal.py
+++ sec2cal.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Usage: Convert sec from reference epoch to calendar components.
-       Input is seconds. Output is year, month, day, hour, minute, second, frac.
+       Input is seconds. Output is year, month, day, hour, minute, second.frac
 """
 usage = __doc__
 
